9dbfb2a986c045703fc4f501a3f3f0c0627bd1b0
Takuma Matsushita <cs14095@gmail.com>
2016-08-05 02:11:34 +0900

    it works in any mode

--- ciel.el
+++ ciel.el
@@ -1,7 +1,18 @@
-;; 
-;; The MIT License (MIT)
+;;; ciel.el --- A command that is clone of "ci" in vim.
 
-;; Copyright (c) 2016 Takuma Matsushita
+;; Copyright (C) 2016 Takuma Matsushita
+
+;; Author: Takuma Matsushita <cs14095@gmail.com>
+;; Created: 2 Jul 2016
+;; Version: 0.0.1
+;; Keywords: convinience
+;; Homepage: https://github.com/cs14095/ciel.el
+;; Package-Requires: ((emacs "24"))
+
+
+;; This file is not part of GNU Emacs
+
+;; The MIT License (MIT)
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
@@ -9,10 +20,10 @@
 ;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 ;; copies of the Software, and to permit persons to whom the Software is
 ;; furnished to do so, subject to the following conditions:
-
+;;
 ;; The above copyright notice and this permission notice shall be included in
 ;; all copies or substantial portions of the Software.
-
+;;
 ;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,107 +31,128 @@
 ;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ;; THE SOFTWARE.
-;;
 
-(defun ci (arg)
+;;; Commentary:
+
+;; You can use ci", ci', ci(, ci{ and ciw entering `Ctrl-c, i`.
+;; Also you can copy them with `Ctrl-c, o` instead of `Ctrl-c, i`.
+;; This is standalone package and you can probably use any mode.
+
+;; **Now at work! I highly recommend updating every week!**
+;; I decided to remove cit on master branch, because it's too heavy.
+;; Other command is still available, but some code is broken.
+;; I will fix in summer vacation...
+
+;; ## Usage
+
+;; Press `Ctrl-c, i` or `Ctrl-c, o` and enter available character.
+;; Watch example or vim usage.
+
+;; ## Example
+
+;; 	Ctrl-c, i, w => kill a word
+;; 	Ctrl-c, i, [<>] => kill inside <>
+;; 	Ctrl-c, i, ' => kill inside ''
+;; 	Ctrl-c, i, " => kill inside ""
+;; 	Ctrl-c, i, [()] => kill inside ()
+;; 	Ctrl-c, i, [{}] => kill inside {}
+;;
+;; 	Ctrl-c, o, w => copy a word
+;; 	Ctrl-c, o, [<>] => copy inside <>
+;; 	Ctrl-c, o, ' => copy inside ''
+;; 	Ctrl-c, o, " => copy inside ""
+;; 	Ctrl-c, o, [()] => copy inside ()
+;; 	Ctrl-c, o, [{}] => copy inside {}
+
+;; you can also kill nested parentheses as you can see.
+;; https://raw.githubusercontent.com/cs14095/cs14095.github.io/master/ci-el.gif
+
+
+;;; Code:
+
+(defun ciel-ci (arg)
   (interactive "sci: ")
   (let ((%region))
-    (cond ((or (string= arg "(") (string= arg ")")) (setq %region (region-paren "(")))
-	  ((or (string= arg "[") (string= arg "]")) (setq %region (region-paren "[")))
-	  ((or (string= arg "{") (string= arg "}")) (setq %region (region-paren "{")))
+    (cond ((or (string= arg "(") (string= arg ")")) (setq %region (ciel-region-paren "(")))
+	  ((or (string= arg "[") (string= arg "]")) (setq %region (ciel-region-paren "[")))
+	  ((or (string= arg "{") (string= arg "}")) (setq %region (ciel-region-paren "{")))
+	  ((or (string= arg "<") (string= arg ">")) (setq %region (ciel-region-paren "<")))
 	  ((or (string= arg "\"")
 	       (string= arg "\'")
 	       (string= arg "\`"))
-	   (setq %region (region-quote arg)))
-	  ((string= arg "w") (setq %region (region-word)))
+	   (setq %region (ciel-region-quote arg)))
+	  ((string= arg "w") (setq %region (ciel-region-word)))
 	  )
     (unless (null %region)
       (kill-region (car %region) (cadr %region)))
     )
   )
-(global-set-key "\C-ci" 'ci)
+(global-set-key "\C-ci" 'ciel-ci)
 
 ;; COpy inside
-(defun co (arg)
+(defun ciel-co (arg)
   (interactive "sco: ")
   (let ((%region))
-    (cond ((or (string= arg "(") (string= arg ")")) (setq %region (region-paren "(")))
-	  ((or (string= arg "[") (string= arg "]")) (setq %region (region-paren "[")))
-	  ((or (string= arg "{") (string= arg "}")) (setq %region (region-paren "{")))
+    (cond ((or (string= arg "(") (string= arg ")")) (setq %region (ciel-region-paren "(")))
+	  ((or (string= arg "[") (string= arg "]")) (setq %region (ciel-region-paren "[")))
+	  ((or (string= arg "{") (string= arg "}")) (setq %region (ciel-region-paren "{")))
+	  ((or (string= arg "<") (string= arg ">")) (setq %region (ciel-region-paren "<")))	  
 	  ((or (string= arg "\"")
 	       (string= arg "\'")
 	       (string= arg "\`"))
-	   (setq %region (region-quote arg)))
-	  ((string= arg "w") (setq %region (region-word)))
+	   (setq %region (ciel-region-quote arg)))
+	  ((string= arg "w") (setq %region (ciel-region-word)))
 	  )
     (unless (null %region)
       (copy-region-as-kill (car %region) (cadr %region)))
     )
   )
 
-(global-set-key "\C-co" 'co)
-
-(defun region-paren (arg)
-  (interactive "s") 
-  (let ((%beginning) (%end) (%target))
-    (move-to-parent-parenthesis arg)
-    (setq %beginning (1+ (point)))
-    (forward-list)
-    (setq %end (1- (point)))
-    (goto-char (1- (point)))
-    (list %beginning %end)
-    )
-  )
-
-;; ( %point% ) => left paren is parent.
-;; ( %point% ( => left paren is parent.
-;; ) %point% ) => right paren is parent.
-;; ) %point% ( => find parent. the t of the second cond form is it.
-(defun move-to-parent-parenthesis (arg)
-  (let ((%target arg) (%init (point)) (%regexp) (%pair))
-    (catch 'process 
-    (cond ((string= %target "(") (setq %regexp "[()]"))
+(global-set-key "\C-co" 'ciel-co)
+(defun ciel-region-paren (arg)
+  (let ((%init (point)) (%beg (point)) (%end (point)) (%fw 0) (%bw 0) (%regexp) (%pair) (%target arg))
+    (cond ((string= %target "(") (setq %regexp "[()]")) ;; for regexp
 	  ((string= %target "{") (setq %regexp "[{}]"))
-	  ((string= %target "[") (setq %regexp "[][]")))
+	  ((string= %target "[") (setq %regexp "[][]"))
+	  ((string= %target "<") (setq %regexp "[<>]")))
     (cond ((string= %target "(") (setq %pair ")"))
 	  ((string= %target "{") (setq %pair "}"))
-	  ((string= %target "[") (setq %pair "]")))
+	  ((string= %target "[") (setq %pair "]"))
+	  ((string= %target "<") (setq %pair ">")))
 
-    (when (string= %target (char-to-string (following-char)))
-      (throw 'process nil)) ;; end here
-    (when (string= %pair (char-to-string (preceding-char)))
-      (backward-list)
-      (throw 'process nil)) ;; end here
+    (cond ((string= %target (char-to-string (following-char))) (setq %beg (point)))
+    	  (t
+    	   (when (string= %pair (char-to-string (preceding-char))) (backward-char))
+    	   (while (not (= %bw 1))
+    	     (re-search-backward %regexp)
+    	     (while (nth 3 (syntax-ppss))
+    	       (re-search-backward %regexp))
+    	     (setq %beg (point))
+    	     (cond ((string= %target (char-to-string (following-char))) (setq %bw (1+ %bw)))
+    		   (t (setq %bw (1- %bw)))))))
     
-    (re-search-backward %regexp)
-    (while (nth 3 (syntax-ppss)) ;; ignore commented
-      (re-search-backward %regexp))
-    (cond ((string= %target (char-to-string (following-char))) ;; backward is (, { or [
-	   ;; do nothing cuz here is parent
-	   )
-	  (t
-	   (goto-char %init)
-	   (re-search-forward %regexp)
-	   (while (nth 3 (syntax-ppss))
-	     (re-search-forward %regexp)) 
-	   (cond ((string= %target (char-to-string (following-char))) ;; forward is (
-		  ;; do nothing
-		  )
-		 (t (let ((%count 0)) ;; here is in the case of ) %point (
-		      (goto-char %init) 
-		      (while (not (= %count 1))
-			(re-search-backward %regexp)
-			(while (nth 3 (syntax-ppss)) ;; ignore commented
-			  (re-search-backward %regexp))
-			(cond ((string= %target (char-to-string (following-char)))
-			       (setq %count(1+ %count)))
-			      (t (setq %count (1- %count))))
-			))))
-	   ))
-    )))
+    (goto-char %init)
+    
+    (cond ((string= %pair (char-to-string (preceding-char))) (setq %end (point)))
+    	  (t
+	   (when (string= %target (char-to-string (following-char))) (forward-char))
+    	   (while (not (= %fw -1))
+    	     (re-search-forward %regexp)
+    	     (while (nth 3 (syntax-ppss))
+    	       (re-search-forward %regexp))
+    	     (setq %end (point))
+    	     (cond ((string= %target (char-to-string (preceding-char))) (setq %fw (1+ %fw)))
+    		   (t (setq %fw (1- %fw)))))))
 
+    ;; adjust pos
+    (setq %beg (1+ %beg))
+    (setq %end (1- %end))
+    (goto-char %beg)
+    (list %beg %end)
+    
+    ))
 
-(defun region-quote (arg)
+(defun ciel-region-quote (arg)
   (let ((%init (point)) (%beg nil) (%end nil) (%fw 0) (%cur (point)))
     (search-backward arg nil t 1)
     (goto-char %init)
@@ -144,7 +176,7 @@
 		   (cond ((< (line-end-position) (match-beginning 0)) (throw 'no-match-in-line-error nil)))
 		   (setq %end (match-beginning 0))
 
-		   (forward-char)
+		   (forward-char) ;; to avoid matching head too
 		   (search-backward arg)
 		   (goto-char %init)
 		   (cond ((> (line-beginning-position) (match-beginning 0)) (throw 'no-match-in-line-error nil)))
@@ -189,7 +221,7 @@
   )
 
 ;; just select word
-(defun region-word ()
+(defun ciel-region-word ()
   (let ((%beginning) (%end) (%init (point)))
     (forward-word 1)
     (setq %beginning (point))
@@ -201,3 +233,4 @@
   )
 
 (provide 'ciel)
+;;; ciel.el ends here
